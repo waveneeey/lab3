a54e3306311027f59333ce5de8239ca68d1eb9fb
vagrant <vagrant@precise32.(none)>
2014-01-23 05:42:37 +0000

    changes for lab 3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -3,6 +3,7 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	$('#submitBtn').click(updatePicture);
 })
 
 /*
@@ -11,8 +12,29 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$(this).text("Button Boss");
+		$('.jumbotron p').toggleClass('bossClass');
 	});
+	$("a.thumbnail").click(projectClick);
+}
+function projectClick(e) {
+	console.log("project clicked");
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00");
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find('.project-description');
+	if (description.length == 0) {
+		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	} else {
+		description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+	};
+}
+function updatePicture(e) {
+	var projectID = $('#project').val();
+    $(projectID).animate({
+    	width: $('#width').val()
+    });
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+    var newText = $('#description').val();
+    $(projectID + " .project-description").text(newText);
 }
